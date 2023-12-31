ebd18de65330cb9daae8a9bb34b43e885e81c33b
Emir Cajic <emir.cajic@source.no>
2023-12-31 03:22:09 +0100

    setup with react-scripts

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@
  */
 
 import React from "react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
-render(<App></App>, document.getElementById("app"));
+ const root = createRoot(document.getElementById('app'));
+ root.render(<App />);
+
